7813d8bb0b8e1562d0eb521be05cdda692efa1d2
Mateusz Ossoliński <mateusz.ossolinski@unitygroup.com>
2023-12-02 15:42:25 +0100

    updated on how data is returned from brands api

--- src/handlers/brands.ts
+++ src/handlers/brands.ts
@@ -1,18 +1,18 @@
 import prisma from '../db'
 
 export const getBrands = async (req, res) => {
-  const brands = await prisma.brand.findMany()
+  const data = await prisma.brand.findMany()
 
-  res.json({ data: brands })
+  res.json({ data })
 }
 
 export const createBrand = async (req, res) => {
-  const brand = await prisma.brand.create({
+  const data = await prisma.brand.create({
     data: {
       name: req.body.name,
       imageUrl: req.body.imageUrl,
     },
   })
 
-  res.json({ data: brand })
+  res.json({ data })
 }
