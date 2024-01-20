e2eb6f4858135330887e0890c2e2b59e2a9a01dd
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2024-01-20 16:28:15 +0100

    set expiration time for jwt token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -15,7 +15,8 @@ export const createJWT = (user) => {
       id: user.id,
       username: user.username,
     },
-    process.env.JWT_SECRET
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' }
   )
 
   return token
